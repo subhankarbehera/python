c0f371898224bab1f694fd7de19b6f88ea9d6a0b
subhankarbehera <75998352+subhankarbehera@users.noreply.github.com>
2020-12-15 17:04:18 +0530

    Add files via upload

---
+++ positve and negative.py
@@ -0,0 +1,11 @@
+list1 = [12,-7,5,64,-14]
+print("original numbers in the list:",list1)
+new_list1 = list(filter(lambda x: x>=0,list1))
+print("positive numbers in the list:",new_list1)
+list2 = [12,14,-95,3]
+print("original numbers in the list:",list2)
+new_list2 = list(filter(lambda x: x>=0,list2))
+print("positive numbers in the list:",new_list2)
+
+
+   
